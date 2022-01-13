5b34a9f657a01ee000177e53b91cdf672d5c24b8
elbasel <abdelrahman.elbasel@gmail.com>
2022-01-13 13:20:53 +0200

    reformatted from/to headers

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -76,7 +76,9 @@ except FileNotFoundError:
 
 # st.title('Real Time Performance')
 
-st.title(f'{account.capitalize()} Egypt Performance {start_date.month}/{start_date.day} to {end_date.month}/{end_date.day}')
+st.title(f'{account.capitalize()} Egypt Performance')
+st.header(f'From: {start_date.month}/{start_date.day}')
+st.header(f'To: {end_date.month}/{end_date.day}')
 st.caption('Made with ❤️ by Abdulrahman Elbasel')
 
 
